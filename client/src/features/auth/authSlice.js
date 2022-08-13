dd9b78cba972aee0cd998945a0bb7f9fedc70db4
Techbrolakes <lekandar11@gmail.com>
2022-08-13 20:50:40 +0100

    Register and Login Functionality Added

---
+++ client/src/features/auth/authSlice.js
@@ -0,0 +1,102 @@
+// createAsyncThunk helps for creating asynchronous functions that can be executed
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import authService from "./authService";
+
+// Get the user from local storage
+const user = JSON.parse(localStorage.getItem("user"));
+
+const initialState = {
+  user: user ? user : null,
+  isError: false,
+  isSuccess: false,
+  isLoading: false,
+  message: "",
+};
+
+// Register the user
+export const register = createAsyncThunk(
+  "api/users",
+  async (user, thunkAPI) => {
+    try {
+      return await authService.register(user);
+    } catch (error) {
+      const message =
+        (error.response &&
+          error.response.data &&
+          error.response.data.message) ||
+        error.message ||
+        error.toString();
+      return thunkAPI.rejectWithValue(message);
+    }
+  }
+);
+
+// Login user
+export const login = createAsyncThunk("auth/login", async (user, thunkAPI) => {
+  try {
+    return await authService.login(user);
+  } catch (error) {
+    const message =
+      (error.response && error.response.data && error.response.data.message) ||
+      error.message ||
+      error.toString();
+    return thunkAPI.rejectWithValue(message);
+  }
+});
+
+// Logout the user
+export const logout = createAsyncThunk("api/logout", async () => {
+  await authService.logout();
+});
+
+export const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  //   Async Actions cant be place in the reducer
+  reducers: {
+    reset: (state) => {
+      state.isError = false;
+      state.isSuccess = false;
+      state.isLoading = false;
+      state.message = "";
+    },
+  },
+  //   Async Actions can be place in the extrareducer
+  extraReducers: (builder) => {
+    builder
+      .addCase(register.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(register.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.isSuccess = true;
+        state.user = action.payload;
+      })
+      .addCase(register.rejected, (state, action) => {
+        state.isLoading = false;
+        state.isError = true;
+        state.message = action.payload;
+        state.user = null;
+      })
+      .addCase(login.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(login.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.isSuccess = true;
+        state.user = action.payload;
+      })
+      .addCase(login.rejected, (state, action) => {
+        state.isLoading = false;
+        state.isError = true;
+        state.message = action.payload;
+        state.user = null;
+      })
+      .addCase(logout.fulfilled, (state) => {
+        state.user = null;
+      });
+  },
+});
+
+export const { reset } = authSlice.actions;
+export default authSlice.reducer;
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,5 +1,10 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
 import { FaUser } from "react-icons/fa";
+import { register, reset } from "../features/auth/authSlice";
+import Spinner from "../components/Spinner";
 
 function Register() {
   const [formData, setFormData] = useState({
@@ -11,6 +16,25 @@ function Register() {
 
   const { name, email, password, password2 } = formData;
 
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const { user, isLoading, isError, isSuccess, message } = useSelector(
+    (state) => state.auth
+  );
+
+  useEffect(() => {
+    if (isError) {
+      toast.error(message);
+    }
+
+    if (isSuccess || user) {
+      navigate("/");
+    }
+
+    dispatch(reset());
+  }, [user, isError, isSuccess, message, navigate, dispatch]);
+
   const onChange = (e) => {
     setFormData((prevState) => ({
       ...prevState,
@@ -18,70 +42,83 @@ function Register() {
     }));
   };
 
-  const onSubmit = async (e) => {
+  const onSubmit = (e) => {
     e.preventDefault();
+
+    if (password !== password2) {
+      toast.error("Passwords do not match");
+    } else {
+      const userData = {
+        name,
+        email,
+        password,
+      };
+
+      dispatch(register(userData));
+    }
   };
+
+  if (isLoading) {
+    return <Spinner />;
+  }
+
   return (
     <>
       <section className="heading">
         <h1>
           <FaUser /> Register
         </h1>
-        <p>Please Create An Account</p>
+        <p>Please create an account</p>
       </section>
 
-      <section>
+      <section className="form">
         <form onSubmit={onSubmit}>
           <div className="form-group">
             <input
               type="text"
+              className="form-control"
               id="name"
               name="name"
               value={name}
               placeholder="Enter your name"
-              className="form-control"
               onChange={onChange}
             />
           </div>
-
           <div className="form-group">
             <input
               type="email"
+              className="form-control"
               id="email"
               name="email"
               value={email}
-              placeholder="Enter your Email"
-              className="form-control"
+              placeholder="Enter your email"
               onChange={onChange}
             />
           </div>
-
           <div className="form-group">
             <input
               type="password"
+              className="form-control"
               id="password"
               name="password"
               value={password}
               placeholder="Enter password"
-              className="form-control"
               onChange={onChange}
             />
           </div>
-
           <div className="form-group">
             <input
               type="password"
+              className="form-control"
               id="password2"
               name="password2"
               value={password2}
-              placeholder="Confirm Password"
-              className="form-control"
+              placeholder="Confirm password"
               onChange={onChange}
             />
           </div>
-
           <div className="form-group">
-            <button type="submit" className="btn btn-block" y>
+            <button type="submit" className="btn btn-block">
               Submit
             </button>
           </div>
